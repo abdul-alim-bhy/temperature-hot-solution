b3f30b03cb099f7b6a99f677e6cf447adb813515
abdul alim bhuiyan <abdulalims2014@gmail.com>
2021-11-17 00:42:00 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const API_KEY = `c172d1b15f316c8af8c10451e65882bf`;
+
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+}
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    console.log(temperature);
+
+    // set weather icon
+
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+
+}
